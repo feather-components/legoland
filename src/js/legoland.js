24533136e98aaf19b4ee6f5cd4ec3eda5045470b
sunjia032 <sunjia032@pingan.com.cn>
2017-02-03 17:22:32 +0800

    add

--- src/js/legoland.js
+++ src/js/legoland.js
@@ -8,15 +8,15 @@ window.onresize = function() {
 
 
 function contentResize() {
-    if (document.getElementsByClassName('lg-s-body-full')[0]) {
-        var bodyH = document.getElementsByClassName('lg-s-body-full')[0].offsetHeight;
+    if (document.getElementsByClassName('lg-body-full')[0]) {
+        var bodyH = document.getElementsByClassName('lg-body-full')[0].offsetHeight;
         var winH = document.body.clientHeight;
-        var navH = document.getElementsByClassName('lg-m-nav')[0].offsetHeight;
-        var breadH = document.getElementsByClassName('lg-m-breadcrumb')[0].offsetHeight;
+        var navH = document.getElementsByClassName('lg-nav')[0].offsetHeight;
+        var breadH = document.getElementsByClassName('lg-breadcrumb')[0].offsetHeight;
         var minH = winH - navH - breadH - 30;
         if (bodyH < minH) {
-            document.getElementsByClassName('lg-s-body-full')[0].style.height = minH + 'px';
-            console.log(bodyH, winH, navH, minH, document.getElementsByClassName('lg-s-body-full')[0].offsetHeight);
+            document.getElementsByClassName('lg-body-full')[0].style.height = minH + 'px';
+            console.log(bodyH, winH, navH, minH, document.getElementsByClassName('lg-body-full')[0].offsetHeight);
         }
     } 
 }
